d6f058fe8fd0a6fbfe37a704a48a642e7a727284
Anson Hoyt <ahoyt@tiu11.org>
2017-11-10 14:21:43 -0500

    GDAL setup for Python

--- .bash/env.sh
+++ .bash/env.sh
@@ -3,3 +3,9 @@ export EDITOR=vim
 
 # Ansible uses cowsay too much for my taste.
 export ANSIBLE_NOCOWS=1
+
+# gdal2 commands, per `brew info osgeo/osgeo4mac/gdal2`
+export PATH="$(brew --prefix)/opt/gdal2/bin:$PATH"
+
+# gdal2-python, per  `brew info osgeo/osgeo4mac/gdal2-python`
+# export PATH="$(brew --prefix)/opt/gdal2-python/bin:$PATH"
