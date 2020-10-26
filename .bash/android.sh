fc3b428eb481c75e803b30d3f05e0f419f595241
Anson Hoyt <ahoyt@tiu11.org>
2020-10-26 08:14:48 -0400

    Updated configs for Android Studio 4. Seems that things have changed.

--- .bash/android.sh
+++ .bash/android.sh
@@ -1,5 +1,9 @@
 # Android (for React Native, etc.)
 # See http://facebook.github.io/react-native/docs/getting-started.html
+# https://reactnative.dev/docs/getting-started.html
+# https://github.com/facebook/react-native/wiki/Building-from-source
+# https://developer.android.com/studio/command-line/variables
 export ANDROID_HOME=~/Library/Android/sdk
+export ANDROID_SDK_ROOT=~/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
