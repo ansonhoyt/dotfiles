08550cb9f1eb46834cdc5578e86cc8da726ee41b
Anson Hoyt <ahoyt@tiu11.org>
2023-03-15 09:38:59 -0400

    Fix brew check for sourcing bash completions.

--- .bash/completions.sh
+++ .bash/completions.sh
@@ -4,7 +4,7 @@
 #
 # See https://docs.brew.sh/Shell-Completion
 
-if type brew 2&>/dev/null; then
+if type brew &>/dev/null; then
   source "$HOMEBREW_PREFIX/etc/bash_completion"
 else
   echo "run: brew install git bash-completion"
