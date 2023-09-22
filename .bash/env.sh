6e06487936d59f348b32adb73cb5773e51a4aab5
Anson Hoyt <ahoyt@tiu11.org>
2023-09-22 07:27:00 -0400

    Remove osgeo4mac/gdal stuff. May not be needed anymore?

--- .bash/env.sh
+++ .bash/env.sh
@@ -11,12 +11,6 @@ export BUNDLER_EDITOR=code
 # Ansible uses cowsay too much for my taste.
 export ANSIBLE_NOCOWS=1
 
-# gdal2 commands, per `brew info osgeo/osgeo4mac/gdal2`
-export PATH="$HOMEBREW_PREFIX/opt/gdal2/bin:$PATH"
-
-# gdal2-python, per  `brew info osgeo/osgeo4mac/gdal2-python`
-# export PATH="$HOMEBREW_PREFIX/opt/gdal2-python/bin:$PATH"
-
 # Display command time when viewing history
 #   "<number> Apr 27 3:45 PM <command>"
 # See `history help; man strftime;`
