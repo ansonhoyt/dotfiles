5516fe00c5a98b9b515ab59f4220a897a76eca59
Anson Hoyt <ahoyt@tiu11.org>
2025-10-19 21:14:10 -0400

    [prompt] Clean out git-prompt.sh remnants, setting a simple prompt (fallback). Move starship to init.sh where it will overwrite the prompt.

--- .bash/init.sh
+++ .bash/init.sh
@@ -7,3 +7,6 @@ eval "$(mise activate bash)"
 
 # https://github.com/ajeetdsouza/zoxide
 eval "$(zoxide init bash)"
+
+# https://starship.rs/
+eval "$(starship init bash)"
--- .bash/prompt.sh
+++ .bash/prompt.sh
@@ -5,24 +5,5 @@
 # \u user
 # \$ shows $ for normal user and # for root user
 
-# Git Prompt
-#-----------------
-# $(__git_ps1) safely gets current git branch. See git-prompt.sh referenced from completions.sh.
-
-# unstaged (*) and staged (+) changes will be shown next to the branch name
-GIT_PS1_SHOWDIRTYSTATE=yes
-
-# If something is stashed, then a '$' will be shown next to the branch name.
-GIT_PS1_SHOWSTASHSTATE=yes
-
-# Show difference between HEAD and its upstream
-GIT_PS1_SHOWUPSTREAM="auto"
-
-# Show colored hint about the current dirty state
-GIT_PS1_SHOWCOLORHINTS=yes
-
-#-----------------
-
-# export PS1='\[\033[0;32m\]\w$(__git_ps1 " (%s)") \$ \[\033[0m\['
-
-eval "$(starship init bash)"
+# Simple prompt
+PS1="\w \u \$ "
