7ed3b8367fb89d9169d3cb025d018f04e8021690
Anson Hoyt <anson.hoyt@gmail.com>
2025-11-08 22:08:55 -0500

    [nvim-lspconfig] Use new vim.lsp.config API (nvim 0.11+) - move LSP setup bits out of init.lua - fix lua LSP warning about using `vim` global

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -21,16 +21,29 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local lspconfig = require("lspconfig")
+      -- Use new vim.lsp.config API (nvim 0.11+)
+      vim.lsp.config.lua_ls = {
+        settings = {
+          Lua = {
+            diagnostics = { globals = { 'vim' } }
+          }
+        }
+      }
 
-      lspconfig.lua_ls.setup({})
-      lspconfig.ts_ls.setup({})
+      vim.lsp.config.ruby_lsp = {
+        cmd = { "ruby-lsp" },
+        filetypes = { "ruby" },
+        root_dir = vim.fs.root(0, { "Gemfile", ".git" })
+      }
+
+      vim.lsp.enable({ 'lua_ls', 'ts_ls', 'ruby_lsp' })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
       vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
+      vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, {})
     end
   }
 }
