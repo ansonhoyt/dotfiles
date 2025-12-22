8eec2c2c8ea9effab3b060ad0dab0bd6760a95e4
Anson Hoyt <ahoyt@tiu11.org>
2025-12-22 15:44:13 -0500

    [ssh] set SSH_AUTH_SOCK to the 1Password SSH Agent's socket

--- .bash/env.sh
+++ .bash/env.sh
@@ -27,3 +27,10 @@ shopt -s histappend
 export HISTCONTROL=ignoreboth # ignorespace + ignoredups
 export HISTSIZE=32768
 export HISTFILESIZE="${HISTSIZE}"
+
+# Setup 1Password SSH Agent communication with other processes.
+# Needed so `ssh-add -l` sees identities.
+# - https://developer.1password.com/docs/ssh/agent/
+# - https://developer.1password.com/docs/ssh/get-started/#step-4-configure-your-ssh-or-git-client
+# - https://blog.joncairns.com/2013/12/understanding-ssh-agent-and-ssh-add/
+export SSH_AUTH_SOCK=~/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
